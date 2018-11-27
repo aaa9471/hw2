2a5e71a54195d9518dcf95327bea895c3a30d4aa
aaa9471 <39412521+aaa9471@users.noreply.github.com>
2018-11-27 10:18:57 +0800

    12

--- hw2.cpp
+++ hw2.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include<queue>
 #include<array>
+#include<cstdio>
 #define infty 3000000
 
 int dirty = 0,clear = 0;
@@ -15,7 +16,7 @@ vector<pair<int,int>> ans;
 class Node
 {
     public:
-    Node(){i=0,j=0,lenth=0,t = 0;};
+    Node(){i=0,j=0,lenth=infty,t = infty;};
     Node(int x,int y,int z,int ti){i=x,j=y,lenth = z,t = ti;};
     Node(int x,int y,int z){i=x,j=y,lenth = z,t = 0;};
     ~Node(){;};
@@ -57,17 +58,22 @@ int  BFS(int i,int j){
     return 1;
 
 }
-
-int mint(int i, int j){
-    int w =(i>0)? map[i-1][j].gett():infty, a = (j>0)?map[i][j-1].gett():infty;
-    int s = (i>row)?map[i+1][j].gett():infty,d =(j<col)?map[i][j+1].gett():infty;
-    int m = (w<s)?w:s, n = a<d?a:d;
+int up,down,rt,lt;
+int mint(int i, int j,int bat){
+    int w = (map[i-1][j].getl()>bat||i<=0)?infty:map[i-1][j].gett();
+    int a = (map[i][j-1].getl()>bat||j<=0)?infty:map[i][j-1].gett();
+    int s = (map[i+1][j].getl()>bat||i>=row-1)?infty:map[i+1][j].gett();
+    int d = (j>=col-1||map[i][j+1].getl()>bat)?infty:map[i][j+1].gett();
+    int m = (w<s)?w:s;
+    int n = a<d?a:d;
+    //cout<<j<<" "<<col<<endl;
+    up = w,down = s, rt = d, lt = a;
     return m<n?m:n;
 }
 
 int maxl(int i,int j,int bat){
     int w =(i>0)? map[i-1][j].getl():0,a =(j>0)? map[i][j-1].getl():0;
-    int s =(i<row)? map[i+1][j].getl():0,d =(j<col)? map[i][j+1].getl():0;
+    int s =(i<row-1)? map[i+1][j].getl():0,d =(j<col-1)? map[i][j+1].getl():0;
     w = (w<bat)?w:0;
     a = (a<bat)?a:0;
     s = (s<bat)?s:0;
@@ -76,36 +82,62 @@ int maxl(int i,int j,int bat){
     int m = (w>s)?w:s, n = (a>d)?a:d;
     return m>n?m:n;
 }
-
+int x=0;
 void clean (int i,int j,int bat,int dir){
-    Node* w = &map[i-1][j],a = &map[i][j-1], s = &map[i+1][j], d = &map[i][j+1];
-    //cout<<i<<","<<j<<","<<bat<<endl;
-    cout<<map[5][6].gett()<<endl;
-    if(clear>=dirty)return;
-    if (j<col &&bat>d->getl()&& d->gett() == mint(i,j) && d->getl() == maxl(i,j,bat)){
+    Node *w = &map[i-1][j],*a = &map[i][j-1], *s = &map[i+1][j], *d = &map[i][j+1];
+    //cout<<map[13][8].gett()<<endl;
+    cout<<i<<","<<j<<","<<bat<<clear<<endl;
+    /*
+    if(i == 11 && j == 7){
+        cout<<mint(i,j,bat)<<" "<<maxl(i,j,bat)<<endl;
+        cout<<w->getl()<<" "<<w->gett()<<endl;
+        printf("%d %d %d %d\n",up,down,rt,lt);
+    }
+    
+
+    //if(maxl(i,j,bat)<map[i][j].getl()&&maxl(i,j,bat)<bat)bat = map[i][j].getl();
+    if(clear==82&& bat == 0){
+        x+=1;
+        //cout<<x<<endl;
+    }
+    */
+    if(clear >= dirty&&bat == 0){
+        /*
+        for(int m =0 ;m<row;m++){
+            for(int n =0;n<col;n++){
+                if(map[m][n].gett()<10)cout<<map[m][n].gett();
+                else if(map[m][n].gett()!=infty)cout<<9;
+                else cout<<"w";
+            }
+            cout<<endl;
+        }
+        */
+        return;
+    }
+    else if (j<col &&bat>d->getl()&& d->gett() == mint(i,j,bat) && d->getl() == maxl(i,j,bat)){
         //cout<<i<<" "<<j<<" "<<d.gett()<<" "<<mint(i,j)<<endl;
         ans.push_back(make_pair(i,j+1));
-        if(!d.gett())clear+=1;
-        d.plustime();
+        if(d->gett() == 0)clear+=1;
+        d->plustime();
         //cout<<d.gett();
         clean(i,j+1,bat-1,6);
     }
-    else if (i>0&&bat>w->getl()&&w->gett()==mint(i,j) && w->getl() == maxl(i,j,bat)){
+    else if (i>0&&bat>w->getl()&&w->gett()==mint(i,j,bat) && w->getl() == maxl(i,j,bat)){
         ans.push_back(make_pair(i-1,j));
         if(!w->gett())clear+=1;
-        w.plustime();
+        w->plustime();
         clean(i-1,j,bat-1,8);
     }
-    else if (j>0&&bat>a.getl()&&a.gett()==mint(i,j)&& a.getl() == maxl(i,j,bat)){
+    else if (j>0&&bat>a->getl()&&a->gett()==mint(i,j,bat)&& a->getl() == maxl(i,j,bat)){
         ans.push_back(make_pair(i,j-1));
-        if(!a.gett())clear+=1;
-        a.plustime();
+        if(!a->gett())clear+=1;
+        a->plustime();
         clean(i,j-1,bat-1,4);
     }
-    else if (i<row&&bat>s.getl()&&s.gett() == mint(i,j)&&s.getl()==maxl(i,j,bat)){
+    else if (i<row&&bat>s->getl()&&s->gett() == mint(i,j,bat)&&s->getl()<=maxl(i,j,bat)){
         ans.push_back(make_pair(i+1,j));
-        if(!s.gett())clear+=1;
-        s.plustime();
+        if(!s->gett())clear+=1;
+        s->plustime();
         //cout<<s.geti()<<" "<<s.getj()<<" "<<s.gett()<<map[4][6].getl()<<endl;
         
         clean(i+1,j,bat-1,2);
@@ -114,38 +146,63 @@ void clean (int i,int j,int bat,int dir){
         switch(dir){
             case 6:
                 ans.push_back(make_pair(i,j-1));
-                clean(i,j-1,29,4);
-                if(!a.gett())clear+=1;
-                a.plustime();
+                clean(i,j-1,battery-1,4);
+                if(!a->gett())clear+=1;
+                a->plustime();
                 break;
             case 8:
                 ans.push_back(make_pair(i+1,j));
-                if(!s.gett())clear+=1;
-                s.plustime();
-                clean(i+1,j,29,2);
+                if(!s->gett())clear+=1;
+                s->plustime();
+                clean(i+1,j,battery-1,2);
                 break;
             case 4:
                 ans.push_back(make_pair(i,j+1));
-                if(!d.gett())clear+=1;
-                d.plustime();
-                clean(i,j+1,29,6);
+                if(!d->gett())clear+=1;
+                d->plustime();
+                clean(i,j+1,battery-1,6);
                 break;
             case 2:
                 ans.push_back(make_pair(i-1,j));
-                if(!w.gett())clear+=1;
-                w.plustime();
-                clean(i-1,j,29,8);
+                if(!w->gett())clear+=1;
+                w->plustime();
+                clean(i-1,j,battery-1,8);
                 break;
+            default:
+                return;
         }
 
     }
+    else if (j>0&&bat>a->getl()&&a->gett()==mint(i,j,bat)&& a->getl() <= maxl(i,j,bat)){
+        ans.push_back(make_pair(i,j-1));
+        if(!a->gett())clear+=1;
+        a->plustime();
+        clean(i,j-1,bat-1,4);
+    }    
+    else if (i>0&&bat>w->getl()&&w->gett()==mint(i,j,bat) && w->getl() <= maxl(i,j,bat)){
+        ans.push_back(make_pair(i-1,j));
+        if(!w->gett())clear+=1;
+        w->plustime();
+        clean(i-1,j,bat-1,8);
+    }
+    else if (j<col &&bat>d->getl()&& d->gett() == mint(i,j,bat) && d->getl() <= maxl(i,j,bat)){
+        //cout<<i<<" "<<j<<" "<<d.gett()<<" "<<mint(i,j)<<endl;
+        ans.push_back(make_pair(i,j+1));
+        if(!d->gett())clear+=1;
+        d->plustime();
+        //cout<<d.gett();
+        clean(i,j+1,bat-1,6);
+    }
     else {
-        //cout<<mint(i,j)<<" "<<maxl(i,j,bat)<<endl;
-        //cout<<a.getl()<<" "<<a.gett()<<endl;
+        cout<<mint(i,j,bat)<<" "<<maxl(i,j,bat)<<endl;
+        cout<<w->getl()<<" "<<w->gett()<<endl;
+        printf("%d %d %d %d\n",up,down,rt,lt);
         cout<<"r"<<endl;
+        cout<<bat<<endl;
+        cout<<clear<<"/"<<dirty<<endl;
         return;
     }
-    
+    return ;
 
 
 }
@@ -167,6 +224,8 @@ int main (int argc,char* argv[]){
     fin>>row>>col>>battery;
     //build map
     // -1 wall 3000000 road 0 r
+    dirty = 0;
+    clear = 0;
     for(int i = 0 ;i<row;i++){
         for(int j = 0;j<col;j++){
             fin>>road;
@@ -176,7 +235,7 @@ int main (int argc,char* argv[]){
                 dirty += 1;
             }
             else if (road == 'R'){
-                map[i][j] = Node(i,j,0,0);
+                map[i][j] = Node(i,j,0,1);
                 i_start = i;
                 j_start = j;
             }
@@ -192,9 +251,8 @@ int main (int argc,char* argv[]){
         fout.close();
         return 0;
     }
-
-    clean(i_start,j_start,battery,30);
-
+    clean(i_start,j_start,battery,0);
+    cout<<clear<<"/"<<dirty<<endl;
     fin.close();
     ofstream fout;
     fout.open(output,ios::out);
